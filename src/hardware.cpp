1eb3e609b1774d53276f9ebe1d24428d3cbf6974
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-09-02 13:47:49 +0200

    Initial commit

---
+++ src/hardware.cpp
@@ -0,0 +1,22 @@
+// =============================================================================
+// Template-Project | Hardware
+// =============================================================================
+
+#include <Arduino.h>
+#include "hardware.h"
+
+namespace Hardware
+{
+
+
+
+    void init()
+    {
+
+    }
+
+    void updateHardware()
+    {
+
+    }
+} // namespace Hardware
