63ff6d1eb82f7a6ec5bca189fe148f6bf447bfca
Dario Casciato <dario.casciato@dormakaba.com>
2023-09-05 14:14:55 +0200

    added first revision of tlv packet creator. also added logger

--- src/tlv/tlv.cpp
+++ src/tlv/tlv.cpp
@@ -1,3 +1,46 @@
 // =============================================================================
 // Arduino-RF-Toolkit | TLV
 // =============================================================================
+
+#include "tlv.h"
+
+
+//------------------------------------------------------------------------------
+// Public methods
+
+TLV::TLVPacket TLV::createPacket(uint16_t tag, uint16_t crc, uint16_t senderAddress, uint16_t receiverAddress, uint8_t* value)
+{
+    TLV::TLVPacket packet;
+    packet.tag = tag;
+    packet.length = TLV::getValueSize(value);
+    packet.crc = crc;
+    packet.senderAddress = senderAddress;
+    packet.receiverAddress = receiverAddress;
+    packet.value = value;
+
+    return packet;
+}
+
+TLV::TLVPacket TLV::parsePacket(char* packet)
+{
+    TLV::TLVPacket parsedPacket;
+    parsedPacket = *(TLV::TLVPacket*)packet;
+
+    return parsedPacket;
+}
+
+//------------------------------------------------------------------------------
+// Private methods
+
+uint16_t TLV::getValueSize(uint8_t* value)
+{
+    return sizeof(value);
+}
+
+uint16_t TLV::getFullPacketSize(TLV::TLVPacket packet)
+{
+    uint16_t packetSize = 0;
+    packetSize = sizeof(packet);
+
+    return packetSize;
+}
