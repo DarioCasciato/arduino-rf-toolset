aad58e527ee9929d4c460c6a3c291cf5f810f676
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-09-07 11:10:12 +0200

    Feature/tlv builder (#1)
    * added first revision of tlv packet creator. also added logger
    
    * added valueLength to tlv
    
    ---------
    
    Co-authored-by: Dario Casciato <dario.casciato@dormakaba.com>

--- src/tlv/tlv.cpp
+++ src/tlv/tlv.cpp
@@ -1,3 +1,41 @@
 // =============================================================================
 // Arduino-RF-Toolkit | TLV
 // =============================================================================
+
+#include "tlv.h"
+
+
+//------------------------------------------------------------------------------
+// Public methods
+
+TLV::TLVPacket TLV::createPacket(uint16_t tag, uint16_t crc, uint16_t senderAddress, uint16_t receiverAddress, uint8_t* value, uint16_t valueLength)
+{
+    TLV::TLVPacket packet;
+    packet.tag = tag;
+    packet.length = valueLength;
+    packet.crc = crc;
+    packet.senderAddress = senderAddress;
+    packet.receiverAddress = receiverAddress;
+    packet.value = value;
+
+    return packet;
+}
+
+TLV::TLVPacket TLV::parsePacket(char* packet)
+{
+    TLV::TLVPacket parsedPacket;
+    parsedPacket = *(TLV::TLVPacket*)packet;
+
+    return parsedPacket;
+}
+
+//------------------------------------------------------------------------------
+// Private methods
+
+uint16_t TLV::getFullPacketSize(TLV::TLVPacket packet)
+{
+    uint16_t packetSize = 0;
+    packetSize = sizeof(packet) + packet.length;
+
+    return packetSize;
+}
--- src/tlv/tlv.h
+++ src/tlv/tlv.h
@@ -1,3 +1,45 @@
 // =============================================================================
 // Arduino-RF-Toolkit | TLV
 // =============================================================================
+
+#include <Arduino.h>
+#include <stdint.h>
+
+
+class TLV
+{
+public:
+    struct TLVPacket
+    {
+        uint16_t tag;
+        uint16_t length;
+        uint16_t crc;
+        uint16_t senderAddress;
+        uint16_t receiverAddress;
+        uint8_t* value;
+    };
+
+    /// @brief Creates a TLV packet. takes uint8_t* as value
+    ///
+    /// @param tag The tag of the TLV packet
+    /// @param crc The crc of the TLV packet
+    /// @param senderAddress The sender address of the TLV packet
+    /// @param receiverAddress The receiver address of the TLV packet
+    /// @param value The value of the TLV packet
+    TLVPacket createPacket(uint16_t tag, uint16_t crc, uint16_t senderAddress,
+                           uint16_t receiverAddress, uint8_t* value, uint16_t valueLength);
+
+    // create a parse tlv packet function
+    /// @brief Parses a TLV packet
+    ///
+    /// @param packet The packet that needs to be parsed
+    /// @return The parsed TLV packet
+    TLVPacket parsePacket(char* packet);
+
+private:
+
+    /// @brief Calculates the full size of the packet
+    /// @param packet The packet that needs to be calculated
+    /// @return The full size of the packet
+    uint16_t TLV::getFullPacketSize(TLVPacket packet);
+};
